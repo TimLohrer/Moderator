1279f202b49f6a2ded68a97c9e2663cdf33fece1
TimLohrer <55639047+TimLohrer@users.noreply.github.com>
2021-09-01 13:05:59 +0200

    time converter for uptie command

--- src/commands/autorole.js
+++ src/commands/autorole.js
@@ -33,9 +33,10 @@ module.exports = {
             if (!role || role === undefined) { return bot.error(`Could not find the role \`${args[0].toString()}\`!`, message) }
             if (role.position > message.guild.roles.cache.find(role => role.name === bot.user.username).position) { return bot.error(`Please move my role (${message.guild.roles.cache.find(role => role.name === bot.user.username)}) **above** ${role} in your server settings! \nThis is necessary in order to use this role as an autorole!`, message, 60) }
             if (role.id === db.autorole) { return bot.error(`${role} is already the autorole!`, message) }
+            const autorole_old = db.autorole
             await firebase.collection('guilds').doc(message.guild.id).update({ autorole: role.id })
-            bot.done(`Changed autorole to ${role}`, message)
-            return bot.logs(`Changed autorole to ${role}`, message)
+            bot.done(`Changed autorole from <@&${autorole_old}> to ${role}`, message)
+            return bot.logs(`Changed autorole from <@&${autorole_old}> to ${role}`, message)
         } else if (db.autorole !== null && args[0] && args[0] === 'none') {
             await firebase.collection('guilds').doc(message.guild.id).update({ autorole: null })
             bot.done(`Disabled autorole!`, message)
--- src/commands/uptime.js
+++ src/commands/uptime.js
@@ -1,3 +1,6 @@
+const { time } = require("console")
+const { uptime } = require("process")
+
 module.exports = {
     name: "uptime",
     aliases: ["uptime", "ut", "upt", "online", "utime"],
@@ -5,8 +8,15 @@ module.exports = {
     description: "Tells you how long the bot is online since the last restart.",
     usage: " ",
     example: " ",
+    cooldown: 30,
     id: 4,
     async execute(message, args, db, bot) {
-        return bot.info(`I am now online since \`${(bot.uptime / 1000 / 60 / 60).toFixed(2)} h\``, message)
+        let time = ""
+        let uptime = bot.uptime
+        if (uptime <= 60 * 1000) { time =  `\`${(uptime / 1000).toFixed(0)} sec\``}
+        else if (uptime >= 60 * 1000 && uptime <= 60 * 1000 * 60) { time = `\`${(uptime / 1000 / 60).toFixed(1)} min\`` }
+        else if (uptime >= 60 * 1000 * 60 && uptime <= 60 * 1000 * 60 * 24) { time = `\`${(uptime / 1000 / 60 / 60).toFixed(0)} h and \`${(uptime / 1000 / 60)} min\`` }
+        else if (uptime >= 60 * 1000 * 60 * 24 && uptime <= 60 * 1000 * 60 * 24 * 365) { time = `\`${(uptime / 1000 / 60 / 60 / 24)} days\` \`${(uptime / 1000 / 60 / 60).toFixed(0)} h and \`${(uptime / 1000 / 60)} min\`` } 
+        return bot.info(`I am now online since ${time}!`, message, 30)
     }
 }
